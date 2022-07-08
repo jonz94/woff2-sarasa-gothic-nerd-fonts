b4848e22c8f53a7f2b3e72c3b99d2f28c6b373f0
jonz94 <jody16888@gmail.com>
2022-07-08 11:12:35 +0800

    🔧 build(config): update `prettier` settings

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,6 +1,9 @@
+/** @type {import('prettier').Config} */
 module.exports = {
   printWidth: 120,
   semi: false,
   singleQuote: true,
   trailingComma: 'all',
+
+  plugins: [require('prettier-plugin-packagejson')],
 }
